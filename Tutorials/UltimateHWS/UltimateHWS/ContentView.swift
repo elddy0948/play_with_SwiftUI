23a3a1251d900e21c5707adb4fcdf83ea1ab47dd
Joons <elddy0948@gmail.com>
2022-07-26 15:18:35 +0900

    [HWSUltimate] : Project init

---
+++ Tutorials/UltimateHWS/UltimateHWS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UltimateHWS
+//
+//  Created by 김호준 on 2022/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tutorials/UltimateHWS/UltimateHWS/UltimateHWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimateHWSApp.swift
+//  UltimateHWS
+//
+//  Created by 김호준 on 2022/07/26.
+//
+
+import SwiftUI
+
+@main
+struct UltimateHWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

b4735780b1584b832427b348ae48150c2a2a0ca3
Joons <elddy0948@gmail.com>
2022-07-28 14:49:41 +0900

    [Bookworm] : Project init and add README.md

---
+++ CoreData/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by 김호준 on 2022/07/28.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
